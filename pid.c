54a529735d930fc63dc9c636eaaefeaee09c205e
Oma-yma1 <zgani.omayma@gmail.com>
2023-05-23 23:48:38 -0700

    editeed

--- pid.c
+++ pid.c
@@ -8,6 +8,7 @@ void pnt_iid(void)
 {
 	char pid_str[100];
 	int pid_len;
+
 	pid_len = sprintf(pid_str, "PID: %d\n", getpid());
 	write(STDOUT_FILENO, pid_str, pid_len);
 }
@@ -19,6 +20,7 @@ void pnt_iiid(void)
 {
 	char ppid_str[100];
 	int ppid_len;
+
 	ppid_len = sprintf(ppid_str, "PPID: %d\n", getppid());
 	write(STDOUT_FILENO, ppid_str, ppid_len);
 }
